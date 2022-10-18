3bfb291a5a5ff3b6661bc87262d6bc2f052fc36f
Ethan Beach <Beached@mail.uc.edu>
2022-10-18 12:00:26 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+import java.util.Random;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        Random rnd = new Random();
+        Boolean gameOver = false;
+        Boolean noPoint = false;
+        String yesNo = "";
+        do{
+            int die1 = rnd.nextInt(6) + 1;
+            System.out.println("First die roles: " + die1);
+            int die2 = rnd.nextInt(6) + 1;
+            System.out.println("Second die roles: " + die2);
+            int crapsRole = die1 + die2;
+            System.out.println("Die Total: " + crapsRole);
+            System.out.println();
+
+            if (crapsRole == 2 || crapsRole == 3 || crapsRole == 12)
+            {
+                System.out.println("You got a craps! Game Over.");
+                System.out.println("Play Again? Y/N");
+                yesNo = in.nextLine();
+                if (yesNo.equalsIgnoreCase("Y"))
+                {
+                    gameOver = false;
+                }
+                else
+                {
+                    gameOver = true;
+                }
+            } else if (crapsRole == 7 || crapsRole == 11)
+            {
+                System.out.println("You got a natural! You Win.");
+                System.out.println("Play Again? Y/N");
+                yesNo = in.nextLine();
+                if (yesNo.equalsIgnoreCase("Y"))
+                {
+                    gameOver = false;
+                }
+                else
+                {
+                    gameOver = true;
+                }
+            }
+            else
+            {
+                System.out.println("You got the point! Rolling Again...");
+                do {
+                    int die3 = rnd.nextInt(6) + 1;
+                    System.out.println("First die roles: " + die3);
+                    int die4 = rnd.nextInt(6) + 1;
+                    System.out.println("Second die roles: " + die4);
+                    int crapsRole2 = die3 + die4;
+                    System.out.println("Die Total: " + crapsRole2);
+                    if (crapsRole2 == 7)
+                    {
+                        System.out.println("You got a 7! Game over.");
+                        noPoint = true;
+                    } else if (crapsRole2 == crapsRole)
+                    {
+                        System.out.println("You got the point! You win.");
+                        noPoint = true;
+                    }
+                    else
+                    {
+                        System.out.println("Not a point or 7. Rolling Again...");
+                        noPoint = false;
+                    }
+                }while(!noPoint);
+                System.out.println("Play Again? Y/N");
+                yesNo = in.nextLine();
+                if (yesNo.equalsIgnoreCase("Y"))
+                {
+                    gameOver = false;
+                }
+                else
+                {
+                    gameOver = true;
+                }
+            }
+        }while(!gameOver);
+    }
+}
